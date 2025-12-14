2af95e0c282a0300051af7e96dd4860dfd4cdc15
TonimaDristy <tidristy477@gmail.com>
2025-12-15 03:01:57 +0600

    headlinecss

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Notification = () => {
     return (
         //<div className='h-12 bg-red-500 text-white'>Free delivery for all orders over $50. Order your food now!</div>
-        <div style={{ backgroundColor: 'red', color: 'white', height: '3rem' }}>
+        <div style={{ backgroundColor: 'maroon', color: 'white', height: '3rem' }}>
             Free delivery for all orders over $50. Order your food now!
         </div>
 
