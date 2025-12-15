d8c8b6c0c9938b0624553269b2b782bbacc95ec2
TonimaDristy <tidristy477@gmail.com>
2025-12-16 01:55:33 +0600

    links.map

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,7 +1,18 @@
-"use client"
+"use client";
 
 import React, { useState } from 'react';
 import Image from 'next/image';
+import { title } from 'process';
+import Link from 'next/link';
+
+const links = [
+
+    { id: 1, title: "Homepage", url: "/" },
+    { id: 2, title: "Menu", url: "/menu" },
+    { id: 3, title: "Working Hours", url: "/" },
+    { id: 4, title: "Contact", url: "/" },
+
+];
 
 const Menu = () => {
     const [open, setOpen] = useState(false)
@@ -12,6 +23,13 @@ const Menu = () => {
             ) : (
                 <Image src="/close.png" alt="Menu" width={20} height={20} onClick={() => setOpen(false)} />
             )}
+            <div>
+                <links.map((item)=>(
+                <Link href={item.url} Key={item.id}>
+                    {item.title
+                    }</Link>
+                ))}
+            </div>
         </div>
     );
 };
