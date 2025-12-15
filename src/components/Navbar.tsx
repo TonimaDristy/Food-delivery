cd41c1049de7b465396583e54a695c3cc4d4d7ec
TonimaDristy <tidristy477@gmail.com>
2025-12-16 00:58:11 +0600

    tailwind fixed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,10 @@ const Navbar = () => {
     return (
         <div className="flex items-center justify-between p-4 border-b-2 border-red-500">
             {/* LOGO */}
-            <Link href="/" className="text-5xl font-extrabold text-red-600 uppercase no-underline">
+            <Link
+                href="/"
+                className="text-6xl font-extrabold text-red-600 uppercase no-underline leading-none"
+            >
                 EatUp
             </Link>
 
